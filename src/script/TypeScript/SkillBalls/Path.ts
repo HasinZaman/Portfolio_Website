f5a0f041005415ba4e39ed31fec8bde54f3d6de3
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2022-03-29 19:28:57 -0400

    Added documentation to Path.ts

--- src/script/TypeScript/SkillBalls/Path.ts
+++ src/script/TypeScript/SkillBalls/Path.ts
@@ -1,6 +1,18 @@
 import { Vector } from "./Vector";
 
+/**
+ * Path interface define the required methods to define a path in 2 dimensions
+ */
 export interface Path{
+    /**
+     * getPoint method should return a Vector on the path
+     * @param {number} t: distance from start point of path 
+     * @returns {Vector} position vector of at t
+     */
     getPoint(t: number) : Vector
+    /**
+     * getCenter method returns center point of Path. The get center method is essential for collision detection
+     * @returns {Vector} position vector of center of path
+     */
     getCenter() : Vector
 }
