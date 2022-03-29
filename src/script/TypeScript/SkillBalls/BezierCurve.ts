1a146a69d517addafe569da1ea1b02d634005d91
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2022-03-29 14:05:24 -0400

    Added documentation to BezierCurve.ts

--- src/script/TypeScript/SkillBalls/BezierCurve.ts
+++ src/script/TypeScript/SkillBalls/BezierCurve.ts
@@ -2,18 +2,33 @@ import { Line } from "./Line";
 import { Path } from "./Path";
 import { Vector } from "./Vector";
 
+/**
+ * BezierCurve class defines a point and methods required to make a bezier curve
+ */
 export class BezierCurve implements Path {
 
+    /**
+     * parameters is an array of positions that define bezier curve
+     */
     parameters : Vector[] = [];
 
+    /**
+     * Constructor creates BezierCurve object
+     * @param parameters 
+     */
     public constructor(parameters : Vector[])
     {
         if(parameters.length < 2) {
-            throw new Error("parameters must have atleast size 2");
+            throw new Error("parameters must have at least size 2");
         }
         this.parameters = parameters;
     }
 
+    /**
+     * getPoint method get a point from bezier curve
+     * @param {number} t: number [0:1] define a point on bezier curve
+     * @returns {Vector} vector position of bezier curve at t
+     */
     public getPoint(t: number): Vector {
         if(t < 0 || 1 < t) {
             throw new Error("out of range [0,1]");
@@ -36,7 +51,7 @@ export class BezierCurve implements Path {
                 ));
             }
 
-            //update paramterTmp with new values at t
+            //update parameterTmp with new values at t
             parameterTmp = [];
             for(let i1 : number = 0; i1 < linesTmp.length; i1++) {
                 parameterTmp.push(linesTmp[i1].getPoint(t));
@@ -46,10 +61,18 @@ export class BezierCurve implements Path {
         return parameterTmp[0];
     }
 
+    /**
+     * getCenter method returns the center point of bezier curve
+     * @returns {Vector} vector position of bezier curve at t
+     */
     public getCenter(): Vector {
         return this.getPoint(0.5);
     }
 
+    /**
+     * Clone method creates a deep copy of BezierCurve object
+     * @returns {BezierCurve} deep copy of BezierCurve
+     */
     public Clone() : BezierCurve{
         let tmp : Vector[] = [];
         this.parameters.forEach(p => {
