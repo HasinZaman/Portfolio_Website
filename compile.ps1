4446ea537b123760b31c43f2e571664e372b47ff
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2022-03-29 23:37:15 -0400

    Update compile.ps1

--- compile.ps1
+++ compile.ps1
@@ -1,5 +1,5 @@
 npm t;
 tsc;
-browserify src/script/TypeScript/SkillBalls/Enviorment.js -o src/script/BallSim.js;
+browserify src/script/TypeScript/SkillBalls/Environment.ts -o src/script/BallSim.js;
 Remove-Item 'src\script\TypeScript\*' -Recurse -Include *.js;
 Remove-Item 'src\script\TypeScript\*' -Recurse -Include *.js.map;
