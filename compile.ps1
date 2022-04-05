07114be43e9b05eba18c9bc2e45065c45fcbb658
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2022-04-05 13:45:12 -0400

    Update compile.ps1

--- compile.ps1
+++ compile.ps1
@@ -1,5 +1,5 @@
 npm t;
 tsc;
-browserify src/script/TypeScript/SkillTable/SkillTable.ts -o src/script/SkillTable.js;
+browserify src/script/TypeScript/SkillTable/SkillTable.js -o src/script/SkillTable.js;
 Remove-Item 'src\script\TypeScript\*' -Recurse -Include *.js;
 Remove-Item 'src\script\TypeScript\*' -Recurse -Include *.js.map;
