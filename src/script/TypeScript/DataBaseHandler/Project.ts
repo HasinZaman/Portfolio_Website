f93194c7d834617af6875e0102199c4b55dfcdf2
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2022-04-17 15:28:31 -0400

    Update Project.ts

--- src/script/TypeScript/DataBaseHandler/Project.ts
+++ src/script/TypeScript/DataBaseHandler/Project.ts
@@ -1,13 +1,47 @@
+import { Tag, TagList } from "./Tag";
+
 /**
  * Project class stores the values related to a project
  */
 export class Project{
-    private _id : number;
     private _primaryTag : number;
-    private _start : {year: number, month : number};
-    private _update : {year: number, month : number};
+
+    public get name() : string {
+        return this.tag.symbol;
+    }
+
+    public get colour() : string {
+        return this.tag.colour;
+    }
+
+    public get tag() : Tag {
+        return TagList.getInstance().tags[this._primaryTag];
+    }
+
+    private _start : Date;
+
+    public get start() : string {
+
+        return new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'long' }).format(this._start);
+    }
+
+    private _update : Date;
+
+    public get update() : string {
+        return new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'long' }).format(this._update);
+    }
+
     private _desc : string;
+
+    public get description() : string {
+        return this._desc;
+    }
+
     private _link : string;
+
+    public get link() : string {
+        return this._link;
+    }
 
     /**
      * Constructor creates Project
@@ -18,8 +52,7 @@ export class Project{
      * @param desc 
      * @param link 
      */
-    constructor (id : number, primaryTag : number, start : {year: number, month : number}, update : {year: number, month : number}, desc : string, link : string) {
-        this._id = id;
+    constructor (primaryTag : number, start : Date, update : Date, desc : string, link : string) {
         this._primaryTag = primaryTag;
         this._start = start;
         this._update = update;
@@ -63,11 +96,39 @@ export class ProjectList {
      */
     public update(listener: () => void) {
         
-        this._projects = [
-            new Project(0, 1, {year: 2014, month: 2}, {year: 2020, month: 2}, "short description", "https://google.com"),
-            new Project(1, 5, {year: 2018, month: 10}, {year: 2022, month: 11}, "short description #2", "https://google.com")
-        ];
+        /*
+        $.ajax({
+            type: "POST",
+            url: "get_data",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            data : JSON.stringify(["project"])
+        }).done(function( dataRaw ) {
+            if (dataRaw.length != 2) {
+                throw Error("Expect one value")
+            }
+            let skillJson = JSON.parse(dataRaw[0])["data"];
+            let connectionsJson = JSON.parse(dataRaw[1])["data"];
 
-        listener();
+            for (let i = 0; i < skillJson.length; i++) {
+                TagList.getInstance().updateSkill(skillJson[i]["id"], skillJson[i]["colour"], skillJson[i]["symbol"], skillJson[i]["tag_type"]);
+            }
+
+            for (let i = 0; i < connectionsJson.length; i++) {
+                TagList.getInstance().updateConnection(connectionsJson[i]["tag_1"], connectionsJson[i]["tag_2"])
+            }
+            listener();
+        })
+        */
+
+        TagList.getInstance().update(() => {
+            this._projects = [
+                new Project(1, new Date(2014, 2), new Date(2020, 2), "short description", "https://google.com"),
+                new Project(2, new Date(2018, 10), new Date(2022, 11), "short description #2", "https://google.com")
+            ];
+    
+            listener();
+        })
     }
 }
