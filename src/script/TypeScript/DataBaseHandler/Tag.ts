dc5cd33a90c20f338050163d4232aff1cfb1a7ca
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2022-04-17 15:26:56 -0400

    Update all refrence to Skill -> Tag in Tag.ts

--- src/script/TypeScript/DataBaseHandler/Tag.ts
+++ src/script/TypeScript/DataBaseHandler/Tag.ts
@@ -47,7 +47,7 @@ export class Tag{
     /**
      * constructor for Skill 
      * @param {number} id: integer representation of id of object
-     * @param {string} colour: hexadecimal representation of skill
+     * @param {string} colour: hexadecimal representation of tag
      * @param {string} symbol: name of symbol string 
      * @param {number} tagType: tagType represents type of tag
      */
@@ -66,32 +66,35 @@ export class TagList{
     private static instance : TagList;
 
     /**
-     * {[key: number]: Tag } skills_: Dictionary of all skills id -> Tag
+     * {[key: number]: Tag } tags_: Dictionary of all tags id -> Tag
      */
-    private skills_ : { [key: number]: Tag } = {};
+    private tags_ : { [key: number]: Tag } = {};
     private keys : number[] = [];
     private connections_ : number[][] = [];
 
     /**
-     * skills getter returns array of tags 
+     * tags getter returns array of tags 
      */
-    get tags() : Tag[] {
-        let skills: Tag[] = [];
+    public get tags() : Tag[] {
+        let tags: Tag[] = [];
         
         for(let i = 0; i < this.keys.length; i++){
-            skills.push(this.skills_[this.keys[i]])
+            tags.push(this.tags_[this.keys[i]])
         }
 
-        return skills;
+        return tags;
     }
 
     /**
      * connections getter returns a 2d array of the connections between tags. let a connection be represented as connections[x][y]; then connections[x] is the list of all the connections that start from skills[x] and connections[x][y] is end point connection skills[y] (skills[x] -> skills[y])
+
+    /**
+     * connections getter returns a 2d array of the connections between tags. let a connection be represented as connections[x][y]; then connections[x] is the list of all the connections that start from tags[x] and connections[x][y] is end point connection tags[y] (tags[x] -> tags[y])
      */
     get connections() : number[][] {
         let map : { [key: number]: number } = {};
 
-        //map tag id -> tag index in skills array
+        //map tag id -> tag index in tags array
         for (let i = 0; i < this.keys.length; i++) {
             map[this.keys[i]] = i;
         }
@@ -105,10 +108,10 @@ export class TagList{
     }
 
     /**
-     * private constructor creates instance of SkillList
+     * private constructor creates instance of TagList
      */
     private constructor() {
-        this.skills_ = {}
+        this.tags_ = {}
         this.connections_ = []
     }
 
@@ -136,16 +139,16 @@ export class TagList{
             headers: {
                 'Content-Type': 'application/json'
             },
-            data : JSON.stringify(["skill", "related"])
+            data : JSON.stringify(["tag", "related"])
         }).done(function( dataRaw ) {
             if (dataRaw.length != 2) {
                 throw Error("Expect one value")
             }
-            let skillJson = JSON.parse(dataRaw[0])["data"];
+            let tagJson = JSON.parse(dataRaw[0])["data"];
             let connectionsJson = JSON.parse(dataRaw[1])["data"];
 
-            for (let i = 0; i < skillJson.length; i++) {
-                TagList.getInstance().updateSkill(skillJson[i]["id"], skillJson[i]["colour"], skillJson[i]["symbol"], skillJson[i]["tag_type"]);
+            for (let i = 0; i < tagJson.length; i++) {
+                TagList.getInstance().updateSkill(tagJson[i]["id"], tagJson[i]["colour"], tagJson[i]["symbol"], tagJson[i]["tag_type"]);
             }
 
             for (let i = 0; i < connectionsJson.length; i++) {
@@ -156,28 +159,28 @@ export class TagList{
     }
 
     /**
-     * updateSkill method update skill with specific id
-     * @param {number} id: id of skill tag that will be updated
+     * updateSkill method update tag with specific id
+     * @param {number} id: id of tag tag that will be updated
      * @param {string} colour 
      * @param {string} symbol 
      * @param {number} tagType 
      */
     public updateSkill(id: number, colour: string, symbol: string, tagType: number) {
-        if (!this.skills_.hasOwnProperty(id)){
+        if (!this.tags_.hasOwnProperty(id)){
             this.keys.push(id);
             this.keys = this.keys.sort((a, b) => {return a - b;});
         }
-        this.skills_[id] = new Tag(id, colour, symbol, tagType);
+        this.tags_[id] = new Tag(id, colour, symbol, tagType);
         
     }
 
     /**
-     * updateConnection connection between two skill tags
+     * updateConnection connection between two tag tags
      * @param id1 
      * @param id2
      */
     public updateConnection(id1: number, id2: number) {
-        if (!this.skills_.hasOwnProperty(id1) || !this.skills_.hasOwnProperty(id2)){
+        if (!this.tags_.hasOwnProperty(id1) || !this.tags_.hasOwnProperty(id2)){
             return;
         }
 
@@ -188,10 +191,10 @@ export class TagList{
     }
 
     /**
-     * resets a skills and connections relationship
+     * resets a tags and connections relationship
      */
     public reset() {
-        this.skills_ = {}
+        this.tags_ = {}
         this.connections_ = []
     }
 }
