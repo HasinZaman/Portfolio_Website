d8b3ca77348863b09a4001bd345f7df565fd04e9
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2022-04-22 01:17:34 -0400

    Update SkillTable.ts

--- src/script/TypeScript/SkillTable/SkillTable.ts
+++ src/script/TypeScript/SkillTable/SkillTable.ts
@@ -153,32 +153,6 @@ function toId(str : string) : string {
         .replace("#","Sharp")
 }
 
-//https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
-function hexToRgb(hex : string) : {r: number, g : number, b: number} {
-    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-    return result ? {
-        r: parseInt(result[1], 16),
-        g: parseInt(result[2], 16),
-        b: parseInt(result[3], 16)
-    } : {
-        r: 0,
-        g: 0,
-        b: 0
-    };
-}
-
-function rgba(col : {r: number, g : number, b: number}, opacity : number ) : {r: number, g : number, b: number} {
-    let calculate = (foreground : number, background : number) => {
-        return foreground * opacity + (1 - opacity) * background
-    }
-
-    return {
-        r : calculate(col.r, 0),
-        g : calculate(col.g, 0),
-        b : calculate(col.b, 0)
-    }
-}
-
 function createOrganizationButton() {
     let target = $("#skills > nav").first();
     let htmlBuilder : HTMLElem = new HTMLElem("div");
