6a4972ad2547deb357f5b7ebd6555362b0e5ee53
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2022-04-30 12:42:08 -0400

    Update Cube.ts
    Cube.ts
     - Fixed html generator in constructor
     - Create front setter
      - setter updates the front most face
     - Created faces getter
      - returns a CubeFaces interface of a jquery objects based on perspective

--- src/script/TypeScript/Cube/Cube.ts
+++ src/script/TypeScript/Cube/Cube.ts
@@ -1,4 +1,5 @@
 import { AttrVal, HTMLElem } from "../HTMLBuilder/HTMLBuilder";
+import { Generator } from "./CycleGroup";
 
 interface CubeFaces {
     front : JQuery,
@@ -11,8 +12,84 @@ interface CubeFaces {
 
 class Cube {
     private cube : JQuery; 
+
+    private static rho = new Generator([3, 2, 0, 1, 4, 5]);//y axis rotation
+    private static phi = new Generator([4, 5, 2, 3, 1, 0]);//x axis rotation
+
+    public get faces() : CubeFaces {
+        let _faces : JQuery[] = [
+            this.cube.find(".front"),
+            this.cube.find(".back"),
+            this.cube.find(".left"),
+            this.cube.find(".right"),
+            this.cube.find(".up"),
+            this.cube.find(".down"),
+        ]
+        switch (this.cube.attr("front")) {
+            case "front":
+                break;
+            case "back":
+                for(let i1 = 0; i1 < 2; i1++) {
+                    _faces = Cube.rho.next(_faces);
+                }
+                break;
+            case "left":
+                for(let i1 = 0; i1 < 1; i1++) {
+                    _faces = Cube.rho.next(_faces);
+                }
+                break;
+            case "right":
+                for(let i1 = 0; i1 < 3; i1++) {
+                    _faces = Cube.rho.next(_faces);
+                }
+                break;
+            case "up":
+                for(let i1 = 0; i1 < 3; i1++) {
+                    _faces = Cube.phi.next(_faces);
+                }
+                break;
+            case "down":
+                for(let i1 = 0; i1 < 0; i1++) {
+                    _faces = Cube.phi.next(_faces);
+                }
+                break;
+        }
+
+        return {
+            front : _faces[0],
+            back : _faces[1],
+            left : _faces[2],
+            right : _faces[3],
+            up : _faces[4],
+            down : _faces[5],
+        };
+    }
+
+    public set front(val : string) {
+        switch(val) {
+            case "front":
+                this.cube.attr("front", "front");
+                break;
+            case "back":
+                this.cube.attr("front", "back");
+                break;
+            case "left":
+                this.cube.attr("front", "left");
+                break;
+            case "right":
+                this.cube.attr("front", "right");
+                break;
+            case "up":
+                this.cube.attr("front", "up");
+                break;
+            case "down":
+                this.cube.attr("front", "down");
+                break;
+        }
+    }
+
     constructor(cube: JQuery) {
-        let cubeHTML : HTMLElem = new HTMLElem("div");
+        let cubeHTML : HTMLElem = new HTMLElem("");
 
         ["front", "back", "left", "right", "up", "down"].forEach(
             (faceClass) => {
@@ -23,10 +100,11 @@ class Cube {
             }
         )
 
-        cube.html(cubeHTML.generate());
+        cube.html(cubeHTML.generateChildren());
         cube.addClass("cube");
         cube.attr("front", "front")
         
         this.cube = cube;
     }
+
 }
