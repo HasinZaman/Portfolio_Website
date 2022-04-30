02690bbf2d1938d51bb1688e63c13346f403d5e3
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2022-04-30 12:42:31 -0400

    Added cube.js to compile.ps1

--- compile.ps1
+++ compile.ps1
@@ -2,5 +2,7 @@ npm t;
 tsc;
 browserify src/script/TypeScript/index.js -o src/script/index.js;
 browserify src/script/TypeScript/Menu/Menu.js -o src/script/Menu.js;
+browserify src/script/TypeScript/Cube/Cube.js -o src/script/Cube.js;
+
 Remove-Item 'src\script\TypeScript\*' -Recurse -Include *.js;
 Remove-Item 'src\script\TypeScript\*' -Recurse -Include *.js.map;
