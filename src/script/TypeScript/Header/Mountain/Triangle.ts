fde4387a0a2e37ff0129cd90b9ef731f19e3959d
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2022-06-06 19:52:01 -0400

    Created Triangle.ts

---
+++ src/script/TypeScript/Header/Mountain/Triangle.ts
@@ -0,0 +1,42 @@
+import { Queue } from "queue-typescript";
+import { Vector } from "../../Math/Vector";
+
+
+const height : number = Math.sqrt(1-0.5*0.5);
+
+function triangle(start: Vector) : Vector[] {
+    let vertices : Vector[] = new Array(3);
+
+    vertices[0] = new Vector(0, 0, 0);
+
+    vertices[1] = new Vector(0.5, 0, -1 * height);
+    vertices[2] = new Vector(-0.5, 0, 0);
+
+    for(let i1 = 0; i1 < vertices.length; i1++) {
+        vertices[i1] = Vector.add(vertices[i1], start);
+    }
+
+    return vertices;
+}
+
+export function triangleFractal(levels : number, start : Vector = new Vector(0,0,0)) : Vector[] {
+    if (levels < 0 || levels %1 != 0) {
+        throw new Error("Invalid level parameter. levels must be from 0 - postive infinity");
+    }
+    let tmp = start.clone();
+
+    let vertices : Vector[] = [];
+
+    let triangleStart : Queue<Vector> = new Queue<Vector>();
+    triangleStart.append(tmp);
+
+    for(let i1 = 0; i1 < levels; i1++) {
+        let triangleVertices : Vector[] = triangle(triangleStart.dequeue());
+
+        triangleVertices.forEach(vector => vertices.push(vector));
+        triangleStart.append(triangleVertices[1]);
+        triangleStart.append(triangleVertices[2]);
+    }
+
+    return vertices;
+}
