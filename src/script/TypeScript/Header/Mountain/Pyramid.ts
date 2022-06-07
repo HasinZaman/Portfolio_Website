8772a6ba062f938884f58c60ac12a06336ae6fa6
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2022-06-07 16:43:05 -0400

    Update Pyramid.ts

--- src/script/TypeScript/Header/Mountain/Pyramid.ts
+++ src/script/TypeScript/Header/Mountain/Pyramid.ts
@@ -1,3 +1,4 @@
+import { Matrix } from "../../Math/Matrix";
 import { Vector } from "../../Math/Vector";
 import { triangleFractal } from "./Triangle";
 
@@ -28,15 +29,54 @@ class Face {
         this._vertices = triangleFractal(levels, new Vector(0,0,0));
     }
 
-    public scale(xScale : number = 1, yScale : number = 1) {
+    private translate(delta : Vector) : void {
+        for(let i1 = 0; i1 < this._vertices.length; i1++) {
+            this._vertices[i1] = Vector.add(this._vertices[i1], delta);
+        }
+    }
 
+    private matrixModify(m : Matrix) {
+        let tmp : Vector = this._vertices[0].clone();
+
+        this.translate(Vector.mult(tmp, -1));
+
+        for(let i1 = 0; i1 < this._vertices.length; i1++) {
+            this._vertices[i1] = Matrix.vectorMult(m, this._vertices[i1]);
+        }
+
+        this.translate(tmp);
+    }
+
+    public scale(xScale: number = 1, yScale: number = 1, zScale: number = 1) {
+
+        this.matrixModify(Matrix.scale(xScale, yScale, zScale));
     }
 
     public rotate(roll: number = 0, pitch: number = 0, yaw: number = 0){
 
+        let rotationMatrix : Matrix = Matrix.mult(
+            Matrix.mult(
+                Matrix.rotationZ(yaw),
+                Matrix.rotationY(pitch)
+            ),
+            Matrix.rotationX(roll)
+        )
+        this.matrixModify(rotationMatrix);
+        this._normal = Matrix.vectorMult(rotationMatrix, this._normal);
     }
 }
 
 class Pyramid {
     private faces : Face[] = [];
+    private center : Vector;
+
+    public constructor(layers: number, heightScale: number, baseScale: number, position: Vector) {
+        this.center = position.clone();
+
+        for(let i1 = 0; i1 < 4; i1++) {
+            this.faces.push(new Face(this.center, layers));
+            this.faces[i1].rotate(0, Math.PI/6, Math.PI/2 * i1)
+            this.faces[i1].scale(baseScale, baseScale, heightScale);
+        }
+    }
 }
