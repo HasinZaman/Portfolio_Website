2e33d8369387d108afd6bc9a3d717b129cd3e334
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2022-06-06 20:21:20 -0400

    Create Pyramid.ts

---
+++ src/script/TypeScript/Header/Mountain/Pyramid.ts
@@ -0,0 +1,38 @@
+import { Vector } from "../../Math/Vector";
+import { triangleFractal } from "./Triangle";
+
+class Face {
+    private _normal: Vector;
+
+    public get normal() : Vector {
+        return this._normal.clone();
+    }
+
+    private set normal(val :  Vector) {
+        this._normal = val;
+    }
+
+    _vertices: Vector[];
+
+    public get vertices(): Vector[] {
+        let tmp : Vector[] = [];
+
+        this._vertices.forEach(val => tmp.push(val.clone()))
+
+        return tmp;
+    }
+
+    public constructor(start: Vector, levels: number){
+        this._normal = new Vector(0, 1, 0);
+
+        this._vertices = triangleFractal(levels, new Vector(0,0,0));
+    }
+
+    public scale(xScale : number = 1, yScale : number = 1) {
+
+    }
+
+    public rotate(roll: number = 0, pitch: number = 0, yaw: number = 0){
+
+    }
+}
