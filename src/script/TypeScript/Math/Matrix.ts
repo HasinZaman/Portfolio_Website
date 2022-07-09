0cf6fe4d9e6b525be7436cf73a3e83e173b9d4f6
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2022-07-09 11:47:09 -0400

    Added JSdoc to Matrix.ts

--- src/script/TypeScript/Math/Matrix.ts
+++ src/script/TypeScript/Math/Matrix.ts
@@ -1,16 +1,27 @@
 import { Vector } from "./Vector";
 
+/**
+ * Matrix class defines methods and variables to create Matrix
+ */
 export class Matrix {
     private values : number[];
 
     private dim : number[];
+
     public get columnCount(): number {
         return this.dim[1];
     };
+
     public get rowCount(): number {
         return this.dim[0];
     };
 
+    /**
+     * add method returns a matrix with the sum of two matrixes
+     * @param {Matrix} m1
+     * @param {Matrix} m2
+     * @returns {Matrix} Matrix with the sum of m1 + m2
+     */
     public static add(m1: Matrix, m2: Matrix) : Matrix {
         if(!(m1.rowCount == m2.rowCount && m1.columnCount == m2.columnCount)) {
             throw new Error("m1 & m2 have invalid size");
@@ -25,10 +36,22 @@ export class Matrix {
         return result;
     }
 
+    /**
+     * sub method returns a matrix with the difference of two matrixes
+     * @param {Matrix} m1 
+     * @param {Matrix} m2 
+     * @returns {Matrix} Matrix with the difference of m1-m2
+     */
     public static sub(m1: Matrix, m2: Matrix) : Matrix {
         return this.add(m1, this.mult(m2, -1));
     }
 
+    /**
+     * mult method returns the Matrix of the product of a scalar value or another matrix
+     * @param {Matrix} v1
+     * @param {Matrix | number} v2
+     * @return {Matrix} Matrix product of v1 * v2
+     */
     public static mult(v1: Matrix, v2: Matrix | number) : Matrix {
 
         if(typeof v2 === "number") {
@@ -38,6 +61,12 @@ export class Matrix {
         return this.matrixMult(v1, v2);
     }
 
+    /**
+     * matrixMult method multiplies two matrixes
+     * @param {Matrix} m1 
+     * @param {Matrix} m2 
+     * @returns {Matrix} Matrix product of two matrixes
+     */
     private static matrixMult(m1: Matrix, m2: Matrix) : Matrix {
         if(!(m1.columnCount == m2.rowCount)) {
             throw new Error("m1 & m2 have invalid size");
@@ -65,6 +94,12 @@ export class Matrix {
         return result;
     }
 
+    /**
+     * scalarMult method returns the product of a matrix and a scalar number
+     * @param {Matrix} m1 
+     * @param {number} s 
+     * @returns {Matrix} Matrix product of Matrix and scalar value
+     */
     private static scalarMult(m1: Matrix, s: number) : Matrix {
         let result : Matrix = new Matrix(m1.rowCount, m1.columnCount);
 
@@ -75,6 +110,12 @@ export class Matrix {
         return result;
     }
 
+    /**
+     * vectorMult method returns the vector product of a matrix and vector
+     * @param {Matrix} m1 
+     * @param {Vector} v 
+     * @returns {Vector} Vector product of Matrix and Vector
+     */
     public static vectorMult(m1: Matrix, v: Vector) : Vector {
         let m2 : Matrix = new Matrix(3, 1);
         m2.setRow(0, [v.x]);
@@ -86,6 +127,11 @@ export class Matrix {
         return new Vector(result.getVal(0, 0), result.getVal(0, 1), result.getVal(0, 2));
     }
 
+    /**
+     * rotationX method generates rotation matrix around the x axis
+     * @param {number} theta 
+     * @returns Rotation matrix around the x axis
+     */
     public static rotationX(theta: number) : Matrix {
         let cos = Math.cos(theta);
         let sin = Math.sin(theta);
@@ -99,6 +145,11 @@ export class Matrix {
         return m;
     }
 
+    /**
+     * rotationY method generates rotation matrix around the y axis
+     * @param {number} theta 
+     * @returns Rotation matrix around the y axis
+     */
     public static rotationY(theta: number) : Matrix {
         let cos = Math.cos(theta);
         let sin = Math.sin(theta);
@@ -112,6 +163,11 @@ export class Matrix {
         return m;
     }
 
+    /**
+     * rotationZ method generates rotation matrix around the z axis
+     * @param {number} theta 
+     * @returns Rotation matrix around the z axis
+     */
     public static rotationZ(theta: number) : Matrix {
         let cos = Math.cos(theta);
         let sin = Math.sin(theta);
@@ -125,6 +181,13 @@ export class Matrix {
         return m;
     }
 
+    /**
+     * scale method generates scaling matrix
+     * @param {number} xScale 
+     * @param {number} yScale 
+     * @param {number} zScale 
+     * @returns {Matrix} scale matrix
+     */
     public static scale(xScale: number = 1, yScale: number = 1, zScale: number = 1) {
         let m : Matrix = new Matrix(3, 3);
 
@@ -135,12 +198,23 @@ export class Matrix {
         return m;
     }
 
+    /**
+     * @constructor creates an instance of Matrix
+     * @param {number} rows
+     * @param {number} columns 
+     */
     constructor(rows: number, columns: number) {
         this.dim = [rows, columns];
 
         this.values = new Array<number>(this.rowCount * this.columnCount).fill(0);
     }
 
+    /**
+     * getVal method gets value from matrix at specific coordinate
+     * @param {number} x 
+     * @param {number} y 
+     * @returns {number} matrix value at (x,y)
+     */
     public getVal(x: number, y: number) : number {
         if(x < 0 || this.columnCount <= x) {
             throw new Error("column out of range");
@@ -153,6 +227,12 @@ export class Matrix {
         return this.values[x + this.columnCount * y];
     }
 
+    /**
+     * setVal method updates value of matrix at (x,y)
+     * @param {number} x 
+     * @param {number} y 
+     * @param {number} val
+     */
     public setVal(x: number, y: number, val : number) : void {
         if(x < 0 || this.columnCount <= x) {
             throw new Error("column out of range");
@@ -165,6 +245,11 @@ export class Matrix {
         this.values[x + this.columnCount * y] = val;
     }
 
+    /**
+     * getRow method returns an array of a row in matrix
+     * @param {number} row 
+     * @returns {number[]} array of numbers in a row
+     */
     public getRow(row: number): number[] {
         let vals : number[] = new Array(this.columnCount);
 
@@ -175,6 +260,11 @@ export class Matrix {
         return vals;
     }
 
+    /**
+     * setRow method updates row in matrix
+     * @param {number} row 
+     * @param {number[]} values 
+     */
     public setRow(row: number, values: number[]) {
         if(values.length != this.columnCount) {
             throw new Error("Invalid size")
@@ -185,6 +275,11 @@ export class Matrix {
         }
     }
 
+    /**
+     * getColumn method returns an array of a column in matrix
+     * @param {number} column 
+     * @returns {number[]} array of numbers in a column
+     */
     public getColumn(column: number): number[] {
         let vals : number[] = new Array(this.rowCount);
 
@@ -195,16 +290,25 @@ export class Matrix {
         return vals;
     }
 
-    public setColumn(col: number, values: number[]) {
+    /**
+     * setColumn method updates column in matrix
+     * @param {number} column 
+     * @param {number[]} values 
+     */
+    public setColumn(column: number, values: number[]) {
         if(values.length != this.rowCount) {
             throw new Error("Invalid size")
         }
 
         for(let y = 0; y < this.rowCount; y++) {
-            this.setVal(col, y, values[y]);
+            this.setVal(column, y, values[y]);
         }
     }
 
+    /**
+     * toString method converts matrix into string representation
+     * @returns {string} string representation of Matrix
+     */
     public toString(): string {
         let tmp : string = "";
 
