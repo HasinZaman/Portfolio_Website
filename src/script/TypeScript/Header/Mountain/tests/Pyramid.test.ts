9703a5ccb9fb86ea6bd6633ca6154c512021a8c5
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2022-08-11 22:14:42 -0400

    Added tests to Pyramid.test.ts
     - Added four unit tests to check - if the pyramid's vertices and faces generate at the correct position; depending on a variety diverse scalar and positional factors

--- src/script/TypeScript/Header/Mountain/tests/Pyramid.test.ts
+++ src/script/TypeScript/Header/Mountain/tests/Pyramid.test.ts
@@ -52,3 +52,67 @@ test("One level pyramid at (1, 5, -7)", () => {
         vectorCheck(actual, Vector.add(basePyramid[index], start));
     })
 })
+
+test("One level pyramid at (0, 0, 0), with base of (2,2) and height of 1", () => {
+    let start : Vector = new Vector(0, 0, 0);
+
+    let pyramid : Pyramid = new Pyramid(1, 1, 2, start);
+
+    let v : Vector[] = pyramid.getVertices(new Vector(0, 0, -1));
+
+    expect(v.length).toBe(3 * 4); 
+
+    v.forEach((actual: Vector, index: number) => {
+        let expected : Vector = basePyramid[index].clone();
+        expected.x *= 2;
+        expected.y *= 2;
+        vectorCheck(actual, expected);
+    })
+})
+
+test("One level pyramid at (0, 0, 0), with base of (2,2) and height of 2", () => {
+    let start : Vector = new Vector(0, 0, 0);
+
+    let pyramid : Pyramid = new Pyramid(1, 2, 2, start);
+
+    let v : Vector[] = pyramid.getVertices(new Vector(0, 0, -1));
+
+    expect(v.length).toBe(3 * 4); 
+
+    v.forEach((actual: Vector, index: number) => {
+        vectorCheck(actual, Vector.mult(basePyramid[index], 2));
+    })
+})
+
+test("One level pyramid at (0, 0, 0), with base of (3,3) and height of 27", () => {
+    let start : Vector = new Vector(0, 0, 0);
+
+    let pyramid : Pyramid = new Pyramid(1, 27, 3, start);
+
+    let v : Vector[] = pyramid.getVertices(new Vector(0, 0, -1));
+
+    expect(v.length).toBe(3 * 4); 
+
+    v.forEach((actual: Vector, index: number) => {
+        let expected : Vector = basePyramid[index].clone();
+        expected.x *= 3;
+        expected.y *= 3;
+        expected.z *= 27;
+        vectorCheck(actual, expected);
+    })
+})
+
+test("One level pyramid at (2, 7, 7), with base of (2,2) and height of 2", () => {
+    let start : Vector = new Vector(2, 7, 7);
+
+    let pyramid : Pyramid = new Pyramid(1, 2, 2, start);
+
+    let v : Vector[] = pyramid.getVertices(new Vector(0, 0, -1));
+
+    expect(v.length).toBe(3 * 4); 
+
+    v.forEach((actual: Vector, index: number) => {
+        let expected : Vector = Vector.add(Vector.mult(basePyramid[index], 2), start)
+        vectorCheck(actual, expected);
+    })
+})
