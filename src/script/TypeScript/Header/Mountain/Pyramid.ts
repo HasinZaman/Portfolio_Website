5d3b99f04546203115a7c2afe0ec8488343c57e5
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2022-08-14 06:58:06 -0400

    Added Documentation to Face & Pyramid in Pyramid.ts

--- src/script/TypeScript/Header/Mountain/Pyramid.ts
+++ src/script/TypeScript/Header/Mountain/Pyramid.ts
@@ -1,7 +1,10 @@
 import { Matrix } from "../../Math/Matrix";
 import { Vector } from "../../Math/Vector";
-import { triangleFractal } from "./Triangle";
+import { triangleFractal, trianglesInFractal } from "./Triangle";
 
+/**
+ * Face class handles the state and management of triangular faces on a pyramid
+ */
 class Face {
     private _normal: Vector;
 
@@ -31,6 +34,22 @@ class Face {
         return tmp;
     }
 
+    /**
+     * rightMostVertexIndex method is used to find the second vertex of silhouette of face
+     * 
+     * 
+     *       1st vertex
+     *          /\
+     *         /  \
+     *        /    \
+     *       /      \
+     *      /        \
+     *     /__________\
+     * 3rd vertex     2nd vertex
+     * 
+     * 
+     * @returns {number} index of vertex that stores the second vertex of silhouette
+     */
     private rightMostVertexIndex() : number {
         let t : number = this._vertices.length / 3;//get number of triangles
 
@@ -47,19 +66,32 @@ class Face {
         return index;
     }
 
+    /**
+     * @constructor creates a triangular fractal face for pyramid
+     * @param {Vector} start vector is the position of the top/tip vertex of the pyramid
+     * @param {number} levels defines the number triangle levels on pyramid
+     */
     public constructor(start: Vector, levels: number){
         this._normal = new Vector(0, 1, 0);
 
         this._vertices = triangleFractal(levels, start);
     }
 
-    private translate(delta : Vector) : void {
+    /**
+     * translate method moves the face in 3d space
+     * @param {Vector} delta 
+     */
+    public translate(delta : Vector) : void {
         for(let i1 = 0; i1 < this._vertices.length; i1++) {
             this._vertices[i1] = Vector.add(this._vertices[i1], delta);
         }
     }
 
-    private matrixModify(m : Matrix) {
+    /**
+     * matrixModify applies a transformation matrix on all the vertices of face
+     * @param {Matrix} m
+     */
+    private matrixModify(m : Matrix) : void {
         let tmp : Vector = this._vertices[0].clone();
 
         this.translate(Vector.mult(tmp, -1));
@@ -71,11 +103,22 @@ class Face {
         this.translate(tmp);
     }
 
-    public scale(xScale: number = 1, yScale: number = 1, zScale: number = 1) {
+    /**
+     * scale method applies a scale matrix transformation on vertices of face
+     * @param {number} xScale 
+     * @param {number} yScale 
+     * @param {number} zScale 
+     */
+    public scale(xScale: number = 1, yScale: number = 1, zScale: number = 1) : void {
         this.matrixModify(Matrix.scale(xScale, yScale, zScale));
     }
 
-    public rotate(pitch: number = 0, yaw: number = 0){
+    /**
+     * rotate method applies a rotation matrix transformation on the Z & X axis
+     * @param {number} pitch 
+     * @param {number} yaw 
+     */
+    public rotate(pitch: number = 0, yaw: number = 0) : void{
 
         let rotationMatrix : Matrix = Matrix.mult(
                 Matrix.rotationZ(yaw),
@@ -86,14 +129,51 @@ class Face {
     }
 }
 
+/**
+ * Pyramid class defines the management and state of vertices of a pyramid. This class is used in the generation of mountains on the header
+ */
 export class Pyramid {
     private _faces : Face[] = [];
     private center : Vector;
 
+    /**
+     * defaultAngle is the radian of the pitch required to make triangular faces of pyramid
+     * 
+     * Pyramid cross section along the ZX axis
+     *         _______
+     *        /|\     |
+     *       /   \    |
+     *      /  |  \   |<- h = sqrt(0.5) 
+     *     /       \  |
+     *    /    |    \ |
+     *   /___________\|
+     *   |     |
+     *   |-----|<- b = 0.5
+     * 
+     * 
+     *  defaultAngle = Math.atan2(b,h)
+     */
     private static defaultAngle : number = 0.6154797086703873;
 
-    public constructor(layers: number, heightScale: number, baseScale: number, position: Vector) {
-        this.center = position.clone();
+    /**
+     * @constructor creates faces of Pyramid with specific characteristics
+     * 
+     * Pyramid cross section along the ZX axis
+     *   start position ->________
+     *                   /\      |
+     *                  /__\     |
+     *                 /\  /\    | <- height
+     *             __ /__\/__\   |
+     *            |  /\  /\  /\  |
+     * 1 layer -> |_/__\/__\/__\_|
+     *              |          |
+     *              |__________| <- base width
+     * 
+     * @param {number} layers
+     * @param {number} height
+     * @param {number} base 
+     * @param {Vector} start 
+     */
     public constructor(layers: number, height: number, base: number, start: Vector) {
         this.center = start.clone();
         
@@ -108,7 +188,12 @@ export class Pyramid {
         }
     }
 
-    public getVertices(cameraDirection : Vector) : Vector[] {
+    /**
+     * getVertices method returns an array of all visible triangles required to draw pyramid
+     * @param {Vector} cameraDirection direction vector of camera
+     * @returns {Vector[]} array of Vectors that define triangles required to be drawn from specific camera orientation. The array can be partitioned in n equally sized partitions of visible faces. Each face partition is formatted in the following format: [v0, v1, v2, v3, v4, v5, ..., vn-2, vn-1, vn]; where (v0, v1, v2) are the triangles required to draw the silhouette for face & (v3, v4, v5)...(vn-2, vn-1, vn) are the triangles required to draw each level
+     */
+    public getTriangles(cameraDirection : Vector) : Vector[] {
         let visibleVectices : Vector[] = [];
 
         this._faces
