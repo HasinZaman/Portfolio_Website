24a9fd4d7ef5dd92b60ea2e830939c38e4d65f57
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2022-08-14 07:10:10 -0400

    Added Documentation to Triangle.ts

--- src/script/TypeScript/Header/Mountain/Triangle.ts
+++ src/script/TypeScript/Header/Mountain/Triangle.ts
@@ -1,9 +1,16 @@
 import { Queue } from "queue-typescript";
 import { Vector } from "../../Math/Vector";
 
+/**
+ * equilateralTriangleHeight constant defines the height of a triangle
+ */
+export const equilateralTriangleHeight : number = Math.sqrt(1 - 0.5*0.5);
 
-export const equilateralTriangleHeight : number = Math.sqrt(1-0.5*0.5);
-
+/**
+ * triangle function find the Vectors required in order to create triangle at start vector
+ * @param {Vector} start 
+ * @returns {Vector[]} Three Vectors required to make a Triangle on the XZ plane
+ */
 function triangle(start: Vector) : Vector[] {
     let vertices : Vector[] = new Array(3);
 
@@ -19,6 +26,20 @@ function triangle(start: Vector) : Vector[] {
     return vertices;
 }
 
+/**
+ * triangleFractal function finds the vectors required to make a triangular fractal on the XZ plane
+ * 
+ *   start position ->
+ *                   /\
+ *                  /__\
+ *                 /\  /\
+ *             __ /__\/__\
+ *            |  /\  /\  /\
+ * 1 level -> |_/__\/__\/__\
+ * @param {number} levels 
+ * @param {Vector} start 
+ * @returns {Vector[]} array of Vectors that define triangles required to be draw the triangleFractal. The array is formatted in the form: [v0, v1, v2, ..., vn-2, vn-1, vn]; where (v0, v1, v2) ... (vn-2, vn-1, vn) define vertices required to make triangle.
+ */
 export function triangleFractal(levels : number, start : Vector = new Vector(0,0,0)) : Vector[] {
     if (levels < 0 || levels %1 != 0) {
         throw new Error("Invalid level parameter. levels must be from 0 - postive infinity");
@@ -54,6 +75,11 @@ export function triangleFractal(levels : number, start : Vector = new Vector(0,0
     return vertices;
 }
 
+/**
+ * trianglesInFractal function finds the number of triangles defined by [triangleFractal]{@link triangleFractal}  function with a certain number of levels
+ * @param {number} levels 
+ * @returns {number} number of triangles
+ */
 export function trianglesInFractal(levels : number) : number {
     return (levels * (levels + 1))/2;
 }
