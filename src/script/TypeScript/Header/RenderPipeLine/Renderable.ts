6dcd62269454c92ae339dbcb140fbdd4dfc78356
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2022-08-15 19:40:19 -0400

    Update Renderable.ts

--- src/script/TypeScript/Header/RenderPipeLine/Renderable.ts
+++ src/script/TypeScript/Header/RenderPipeLine/Renderable.ts
@@ -1,4 +1,6 @@
+import { HTMLElem } from "../../HTMLBuilder/HTMLBuilder";
 import { Vector } from "../../Math/Vector";
+
 
 /**
  * Renderable interface defines methods and states that would be required in order to render an object
@@ -6,6 +8,14 @@ import { Vector } from "../../Math/Vector";
 export interface Renderable {
     /**
      * getTriangles method returns an array of triangles that would be visible. The method is very similar to what a vertex shader.
+     * @param {Vector} cameraDir
+     * @param {Vector} cameraPos
+     * @returns {Vector[]}
      */
-    getTriangles : (cameraDir: Vector, cameraPos: Vector) => Vector[]
+    getTriangles : (cameraDir: Vector, cameraPos: Vector) => Vector[];
+
+    /**
+     * draw method returns an HTMLElem to be inserted into an svg element to generate an image
+     */
+    draw: (tmp: Vector[]) => HTMLElem
 }
