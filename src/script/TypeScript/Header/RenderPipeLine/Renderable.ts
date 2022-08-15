cab1a5061cbc44fc18a0c06b0e8df90524b840c5
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2022-08-15 18:20:00 -0400

    Update Renderable.ts

--- src/script/TypeScript/Header/RenderPipeLine/Renderable.ts
+++ src/script/TypeScript/Header/RenderPipeLine/Renderable.ts
@@ -5,7 +5,7 @@ import { Vector } from "../../Math/Vector";
  */
 export interface Renderable {
     /**
-     * getTriangles method returns an array of triangles that would be visible
+     * getTriangles method returns an array of triangles that would be visible. The method is very similar to what a vertex shader.
      */
     getTriangles : (cameraDir: Vector, cameraPos: Vector) => Vector[]
 }
