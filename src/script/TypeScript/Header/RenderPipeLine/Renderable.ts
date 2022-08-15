447228ebde003e890f7d4195a07f03614e12194d
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2022-08-15 10:27:56 -0400

    Create Renderable.ts

---
+++ src/script/TypeScript/Header/RenderPipeLine/Renderable.ts
@@ -0,0 +1,11 @@
+import { Vector } from "../../Math/Vector";
+
+/**
+ * Renderable interface defines methods and states that would be required in order to render an object
+ */
+export interface Renderable {
+    /**
+     * getTriangles method returns an array of triangles that would be visible
+     */
+    getTriangles : (cameraDirection : Vector) => Vector[]
+}
